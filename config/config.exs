2d29e75a5f2415a0865dd312e21048b833893021
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-04-10 14:38:32 -0500

    ci: roll out shared github-actions workflows

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,33 @@
 import Config
 
+if config_env() == :dev do
+  config :git_hooks,
+    auto_install: true,
+    verbose: true,
+    hooks: [
+      commit_msg: [
+        tasks: [
+          {:cmd, "mix git_ops.check_message", include_hook_args: true}
+        ]
+      ]
+    ]
+
+  config :git_ops,
+    mix_project: Jido.Messaging.MixProject,
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/agentjido/jido_messaging",
+    manage_mix_version?: true,
+    version_tag_prefix: "v",
+    types: [
+      feat: [header: "Features"],
+      fix: [header: "Bug Fixes"],
+      perf: [header: "Performance"],
+      refactor: [header: "Refactoring"],
+      docs: [hidden?: true],
+      test: [hidden?: true],
+      chore: [hidden?: true],
+      ci: [hidden?: true]
+    ]
+end
+
 import_config "#{Mix.env()}.exs"
