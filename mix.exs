cbee634d1052a5e5d5c60ec049f39c65c4004f14
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-04-21 16:07:19 -0500

    fix: use standalone jido_chat dependency

--- mix.exs
+++ mix.exs
@@ -68,7 +68,7 @@ defmodule Jido.Messaging.MixProject do
   defp deps do
     [
       # Runtime dependencies
-      {:jido_chat, path: "../jido_chat"},
+      {:jido_chat, github: "agentjido/jido_chat", branch: "main"},
       {:jason, "~> 1.4"},
       {:zoi, "~> 0.14"},
       {:jido, "~> 2.1"},
